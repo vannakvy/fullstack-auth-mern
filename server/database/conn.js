ffbe67bab0de2efd896dd0296b2708f2ea0a1987
vannakvy <vannakvy2020@gmail.com>
2023-03-17 14:47:29 +0700

    fix url

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
-import { MongoMemoryServer } from "mongodb-memory-server";
-// import ENV from "../config.js";
+// import { MongoMemoryServer } from "mongodb-memory-server";
+import ENV from "../config.js";
 
 async function connect() {
-  const mongod = await MongoMemoryServer.create();
-  const getUri = mongod.getUri();
-  console.log("htis", getUri);
+  // const mongod = await MongoMemoryServer.create();
+  // const getUri = mongod.getUri();
+  // console.log("htis", getUri);
   mongoose.set("strictQuery", true);
-  //   const db = await mongoose.connect(getUri);
-  const db = await mongoose.connect(getUri);
+  const db = await mongoose.connect(ENV.ATLAS_URI);
+  // const db = await mongoose.connect(getUri);
+  console.log(db);
   console.log("Database Connected");
   return db;
 }
